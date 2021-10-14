c80ab644ddcfa5189192ba47d4c94d9c717ebfa9
Ralf Claussnitzer <ralf.claussnitzer@gmail.com>
2021-10-14 10:22:00 +0200

    Add simple sub form

--- src/Form/HelloForm.php
+++ src/Form/HelloForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -18,9 +19,8 @@ class HelloForm extends AbstractType
         $builder
             ->add('title', TextType::class)
             ->add('content', TextareaType::class)
-
             ->add('email', EmailType::class)
-            
+            ->add($builder->create('subform', FormType::class)->add('name', TextType::class))
             ->add('submit', SubmitType::class)
             ->add('reset', ResetType::class);
     }
